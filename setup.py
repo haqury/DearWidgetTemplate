526e3bf6d28b3ae67439ea5e25e09ca56d98e33d
haqur <haqur@HAOSIT>
2023-12-31 22:01:29 +0400

    init

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     author_email='haqury@gmail.com',
     packages=['DearWidgetTemplate'],  # same as name
     install_requires=['pyperclip', 'PyQt5', 'sys', 'win32api',
-                      'DearWidgetTemplate @ git+https://github.com/haqury/SpeachToTextWidget.git@v1.0#egg=DearWidgetTemplate'
+                      'SpeachToTextWidget @ git+https://github.com/haqury/SpeachToTextWidget.git@v1.0#egg=SpeachToTextWidget'
                       ],  # external packages as dependencies
 )
